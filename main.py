e637795925916ef514c76ac23be4e95ca4535899
rishabh16-2005 <rishabhgalave@gmail.com>
2025-06-17 17:58:23 +0530

    Implement data transformation functionality and related configurations

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from networksecurity.components.data_ingestion import DataIngestion
 from networksecurity.components.data_validation import DataValidation
+from networksecurity.components.data_transformation import DataTransformation
 from networksecurity.exception.exception import NetworkSecurityException
 from networksecurity.logging.logger import logging
 from networksecurity.entity.config_entity import TraingPipelineConfig
-from networksecurity.entity.config_entity import DataIngestionConfig , DataValidationConfig
+from networksecurity.entity.config_entity import DataIngestionConfig , DataValidationConfig,DataTransformationConfig
 import sys
 if __name__ == "__main__":
     try:
@@ -16,7 +17,13 @@ if __name__ == "__main__":
         data_validation_config = DataValidationConfig(training_pipeline_config=trainingpipelineconfig)
         data_validation = DataValidation(dataingestionartifacts,data_validation_config)
         logging.info("Initiate Data Validation")
-        data_validation.initiate_data_validation()
+        data_validation_artifact = data_validation.initiate_data_validation()
         logging.info("Data Validation Completed")
+        data_transformation_config = DataTransformationConfig(training_pipeline_config=trainingpipelineconfig)
+        data_transformation = DataTransformation(data_validation_artifact=data_validation_artifact,data_transformation_config=data_transformation_config)
+        data_transformation_artifact = data_transformation.initiate_data_transformation()
+        logging.info("Data Transformation Completed")
+        print(data_transformation_artifact)
     except Exception as e:
         raise NetworkSecurityException(e,sys)
+    
